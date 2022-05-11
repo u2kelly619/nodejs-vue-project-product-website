5f37bba681a0e37fd39cfff6bffb9030456091d0
Kelly Chang <u2kelly619@gmail.com>
2022-05-11 13:57:29 +0800

    add cowsay and nodemon module

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-console.log("hello world");
+// console.log("hello world");
 
+//第一個區塊，內建模組
+const path = require('path');
+const http = require('http');
+
+//第二個區塊，第三方模組(套件)
+
+//第三個區塊，自建模組
 const hello = require("./hello.js");
 
 hello.sayHello();
 hello.sayGoodNight();
+
+const cowsay = require('cowsay');
+
+let sentences = ['哈囉', '安安', '我是牛牛'];
+
+sentences.forEach((sentence) => {
+    console.log(cowsay.say({
+        text : sentence,
+        e : "^^",
+        T : "U "
+    }));
+});
